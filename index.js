ec44409164742930337d3ed3484947b258c1d224
Ash <41401030+Ashatiel@users.noreply.github.com>
2022-12-08 02:11:58 -0800

    Fix Directories

--- index.js
+++ index.js
@@ -9,10 +9,10 @@ client.commands = new Collection();
 client.color = "FFFFFF";
 client.commandArray = [];
 
-const functionFolders = fs.readdirSync(`./src/functions`);
+const functionFolders = fs.readdirSync(`./functions`);
 for (const folder of functionFolders) {
   const functionFiles = fs
-    .readdirSync(`./src/functions/${folder}`)
+    .readdirSync(`./functions/${folder}`)
     .filter((file) => file.endsWith(".js"));
   for (const file of functionFiles)
     require(`./functions/${folder}/${file}`)(client);
